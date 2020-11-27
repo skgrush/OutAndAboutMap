052db5e617757665c0f314c3bd30b2608da773c9
Samuel K. Grush <Samuel@Grush.org>
2020-11-26 20:50:41 -0600

    Initial Commit

---
+++ OutAndAboutMap/OutAndAboutMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  OutAndAboutMapApp.swift
+//  OutAndAboutMap
+//
+//  Created by Samuel Grush on 11/26/20.
+//
+
+import SwiftUI
+
+@main
+struct OutAndAboutMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
